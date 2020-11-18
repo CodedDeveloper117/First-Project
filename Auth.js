a6dd04ec9d5da1a95312ab8010797ca7e95ae197
EmmanuelDev <58920970+Emmanuel112673@users.noreply.github.com>
2020-11-18 10:55:38 +0100

    Logout function

---
+++ Auth.js
@@ -0,0 +1,15 @@
+class Auth {
+    
+    constructor(){
+
+    }
+    login(){
+        console.log('Logged In');
+    }
+    signUp(){
+        console.log('Signed Up')
+    }
+    logout(){
+        console.log('Logged Out...')
+    }
+}
